509e46fca8e5923325f584da8adbc7f4b33e4f45
victor.baggio <baggiobaggiovictor@gmail.com>
2022-12-07 18:12:50 -0300

    feat: add total hours

--- src/app/pages/for-hour/students/student/student.component.ts
+++ src/app/pages/for-hour/students/student/student.component.ts
@@ -1,7 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { Student } from '../../../../interfaces/student.interface';
+import { CertificatesService } from 'src/app/services/certificates.service';
+import { map } from 'rxjs';
+import { Certificate } from 'src/app/interfaces/certificate.interface';
+
+type CertificateResponse = {
+  student: Student;
+  certificates: Certificate[];
+};
 
 @Component({
   selector: 'app-student',
@@ -10,10 +18,33 @@ import { Student } from '../../../../interfaces/student.interface';
   standalone: true,
   imports: [CommonModule, RouterLinkWithHref],
 })
-export class StudentComponent {
+export class StudentComponent implements OnInit {
   @Input() student!: Student;
 
   // define algumas estruturas do css
   // index  do array
   @Input() index!: number;
+
+  public validatedHours: number = 0;
+
+  constructor(private certificateService: CertificatesService) {}
+
+  ngOnInit(): void {
+    this.certificateService
+      .getAllByStudent(this.student.id)
+      .pipe(
+        map((response: CertificateResponse) =>
+          response.certificates.filter(
+            (_: Certificate) => _?.isValidated === true
+          )
+        )
+      )
+      .subscribe(
+        (certificates) =>
+          (this.validatedHours = certificates.reduce(
+            (sum, current) => sum + current.hours,
+            0
+          ))
+      );
+  }
 }
